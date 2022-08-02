41185c8c9351eccffbc9cc651671cddf9d6e47e3
NasimbayevFarhod <105714502+NasimbayevFarhod@users.noreply.github.com>
2022-08-02 22:25:45 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var elModalOpenButton = document.querySelector(".menu");
+var elnav = document.querySelector(".navbar__close");
+
+
+elModalOpenButton.addEventListener("click", function(){
+  elnav.classList.toggle("navbar");
+})
+
+
+
+
